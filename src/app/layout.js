d1ea781ea2eaae2f2d455abe0690679dddfcc56a
Dhruvkumar patel <Iamdhruv1563@gmail.com>
2023-06-01 14:27:21 +0530

    added the og meta information and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <head></head>
+            <head>
+                <meta property="og:title" content="Dhruvkumar's Portfolio" />
+                <meta property="og:site_name" content="stack-dhruv" />
+                <meta property="og:url" content="stack-dhruv.tech" />
+                <meta
+                    property="og:description"
+                    content="Dhruvkumar Patel is a final year IT engineer with a passion for using technology to solve real-world problems. His website portfolio showcases his skills in web development, software development, and data science. Visit his website today to learn more about his work!"
+                />
+                <meta property="og:type" content="article" />
+                <meta
+                    property="og:image"
+                    content="https://images.unsplash.com/photo-1685452221648-9b5b870b9195?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
+                />
+            </head>
             <body
                 className={`${inter.className} bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}
                 // style="--fixation-edge-opacity: 80%; --br-line-height: 1; --br-boldness: 600;"
